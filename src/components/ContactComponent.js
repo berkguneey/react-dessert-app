be86853734b79977434f5c880ff6bd76cc9c786d
Berk Güney <berkcanguney@gmail.com>
2021-04-25 19:19:01 +0300

    contact component

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from "react-router-dom";
 
 function Contact(props) {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+  const handleSubmit = (e) => {
+    console.log(name + email + message);
+    e.preventDefault();
+  };
   return (
     <div className="row-content">
       <div className="row">
@@ -16,25 +23,39 @@ function Contact(props) {
         </div>
       </div>
       <div className="row">
-        <div className="col-md-12">
-          <form id="contact-form" method="POST">
+        <div className="col-md-8">
+          <form id="contact-form" method="POST" onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control"
+              />
             </div>
             <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
+              <label htmlFor="email">Email Address</label>
               <input
                 type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="form-control"
-                aria-describedby="emailHelp"
               />
             </div>
             <div className="form-group">
               <label htmlFor="message">Message</label>
-              <textarea className="form-control" rows="5"></textarea>
+              <textarea
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className="form-control"
+                rows="5"
+              ></textarea>
             </div>
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary btn-block">
               Submit
             </button>
           </form>
